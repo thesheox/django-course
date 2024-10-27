3d908cf1d30fa49f045ea04fa9248c9f09961189
thesheox <thesheox@gmail.com>
2024-10-27 10:25:09 +0330

    first

---
+++ website/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+def index_view(request):
+    return render(request, 'website/index.html')
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
